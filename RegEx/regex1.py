251c5c2865862d3790285933819797dfda888323
KU <johndoe@example.com>
2025-07-31 21:53:57 +0530

    regex examples

---
+++ RegEx/regex1.py
@@ -0,0 +1,14 @@
+# Write a Python program that checks if the word "hello" (case-sensitive) is present anywhere in a given string. Print "Found!" if it is, otherwise print "Not found.".
+
+import re
+
+sentence = input("Enter a sentence: ")
+print (sentence)
+
+"""
+if "hello" in sentence:
+    print ("Found")
+    
+else:
+    print ("Not found")
+"""
